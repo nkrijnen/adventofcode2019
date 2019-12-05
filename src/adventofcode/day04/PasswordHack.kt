7346163da30b156b34bcefbebf271d1b07db6349
Nico Krijnen <nkr@woodwing.com>
2019-12-05 20:13:57 +0100

    Day 4 part 2

--- src/adventofcode/day04/PasswordHack.kt
+++ src/adventofcode/day04/PasswordHack.kt
@@ -2,15 +2,8 @@ package adventofcode.day04
 
 fun IntRange.possiblePasswordCount(): Int = this.count(Int::isValidPassword)
 
-internal fun Int.isValidPassword() = this.hasTwoAdjacentSameDigits() && this.digitsNeverDecrease()
-
-internal fun Int.hasTwoAdjacentSameDigits(): Boolean {
-    val chars = this.toString().toCharArray()
-    for (idx in 1 until chars.size) {
-        if (chars[idx - 1] == chars[idx]) return true
-    }
-    return false
-}
+internal fun Int.isValidPassword() =
+    this.hasTwoAdjacentSameDigits() && this.digitsNeverDecrease()
 
 internal fun Int.digitsNeverDecrease(): Boolean {
     val digits = this.toString().toCharArray().map { it.toInt() }
@@ -20,3 +13,32 @@ internal fun Int.digitsNeverDecrease(): Boolean {
     return true
 }
 
+internal fun Int.hasTwoAdjacentSameDigits(): Boolean {
+    val chars = this.toString().toCharArray()
+    for (idx in 1 until chars.size) {
+        if (chars[idx - 1] == chars[idx]) return true
+    }
+    return false
+}
+
+// part 2
+
+fun IntRange.possibleStrictPasswordCount(): Int = this.count(Int::isStrictlyValidPassword)
+
+internal fun Int.isStrictlyValidPassword() =
+    this.hasAtLeastOneSetOfTwoAdjacentSameDigits() && this.digitsNeverDecrease()
+
+internal fun Int.hasAtLeastOneSetOfTwoAdjacentSameDigits(): Boolean {
+    val chars = this.toString()
+    var streakStart = 0
+    while (streakStart < chars.length - 1) {
+        var streakLength = 1
+        for (streakEnd in streakStart + 1 until chars.length) {
+            if (chars[streakStart] == chars[streakEnd]) streakLength++
+            else break
+        }
+        if (streakLength == 2) return true
+        streakStart += streakLength
+    }
+    return false
+}
--- test/adventofcode/day04/PasswordHackTest.kt
+++ test/adventofcode/day04/PasswordHackTest.kt
@@ -23,4 +23,20 @@ internal class PasswordHackTest {
         val range = 109165..576723
         assertEquals(2814, range.possiblePasswordCount())
     }
+
+    @Test
+    fun `sample cases part 2`() {
+        assertFalse(111112.isStrictlyValidPassword())
+        assertFalse(123444.isStrictlyValidPassword())
+        assertTrue(112233.isStrictlyValidPassword())
+        assertTrue(111122.isStrictlyValidPassword())
+    }
+
+    @Test
+    fun `how many different 'strict' passwords`() {
+        val range = 109165..576723
+        // range.filter { it.isStrictlyValidPassword() }.forEach(::println)
+        assertNotEquals(2581, range.possibleStrictPasswordCount())
+        assertEquals(1991, range.possibleStrictPasswordCount())
+    }
 }

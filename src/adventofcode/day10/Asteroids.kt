a97b9bc691c18348f076113deb1c79f4ffe0ef35
Nico Krijnen <nkr@woodwing.com>
2019-12-10 23:11:44 +0100

    Day 10 - part 1
    Remove cruft

--- src/adventofcode/day10/Asteroids.kt
+++ src/adventofcode/day10/Asteroids.kt
@@ -3,17 +3,6 @@ package adventofcode.day10
 import java.awt.geom.Line2D
 import java.awt.geom.Point2D
 
-//.A..#
-//.....
-//###Zz
-//....#
-//...##
-//z d = 0.0 for Line(start=Coordinate(x=1, y=0), end=Coordinate(x=4, y=3)), blocking: Coordinate(x=3, y=2)
-
-
-//s d = 0.0 for Line(start=Coordinate(x=1, y=0), end=Coordinate(x=3, y=4)), blocking: Coordinate(x=2, y=2)
-
-
 data class Coordinate(val x: Int, val y: Int) {
     fun lineTo(other: Coordinate) = Line(this, other)
     internal fun toPoint(): Point2D = Point2D.Double(x.toDouble(), y.toDouble())
@@ -21,12 +10,7 @@ data class Coordinate(val x: Int, val y: Int) {
 }
 
 data class Line(val start: Coordinate, val end: Coordinate) {
-    //    fun isOnSegment(point: Coordinate): Boolean = toLine().ptSegDist(point.toPoint()) == 0.0
-    fun isOnSegment(point: Coordinate): Boolean {
-        val ptSegDist = toLine().ptSegDist(point.toPoint())
-//        println("  d = $ptSegDist for $this, blocking: ${point}")
-        return ptSegDist == 0.0
-    }
+    fun isOnSegment(point: Coordinate) = toLine().ptSegDist(point.toPoint()) == 0.0
 
     private fun toLine() = Line2D.Double(start.toPoint(), end.toPoint())
 
@@ -36,26 +20,16 @@ data class Line(val start: Coordinate, val end: Coordinate) {
 
 class Scan(private val asteroids: Set<Coordinate>) {
     fun nrOfDetectableAsteroidsFromBestLocation(): Int = asteroids.map { visibleAsteroids(it) }.max()!!
-    // for possibleStation in asteroids
-    //   count possiblyVisible in asteroids without possibleStation
-    //     exclude any if block view: check possiblyBlocking in asteroids without possibleStation and possiblyVisible
-    //         if in direct path between possibleStation and possiblyVisible
+
     private fun visibleAsteroids(station: Coordinate): Int {
         val possiblyVisible = asteroids.filterNot { it == station }
-        val count = possiblyVisible.count { target ->
+        return possiblyVisible.count { target ->
             val lineOfSight = station.lineTo(target)
             val possiblyBlocking = possiblyVisible.filterNot { it == target }
-            val blocking = possiblyBlocking.firstOrNull { lineOfSight.isOnSegment(it) }
-            if (blocking != null)
-                println("line(${station.x},${station.y}, ${target.x},${target.y}); line(${station.x},${station.y}, ${blocking.x},${blocking.y}, True)")
-//            println("  for $lineOfSight, blocking: $blocking")
             possiblyBlocking.all { it isNotBlocking lineOfSight }
         }
-        println("#for $station, $count")
-        return count
     }
 }
-
 
 fun String.toScan(): Scan = Scan(this.trimIndent()
     .lines().mapIndexed { y, line ->

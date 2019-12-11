33f0ae14e5d16323d66439d9c2b012e09edf0c63
Nico Krijnen <nkr@woodwing.com>
2019-12-11 19:55:05 +0100

    Day 10 - part 2

--- src/adventofcode/day10/Asteroids.kt
+++ src/adventofcode/day10/Asteroids.kt
@@ -2,11 +2,19 @@ package adventofcode.day10
 
 import java.awt.geom.Line2D
 import java.awt.geom.Point2D
+import kotlin.math.PI
+import kotlin.math.atan2
 
 data class Coordinate(val x: Int, val y: Int) {
     fun lineTo(other: Coordinate) = Line(this, other)
     internal fun toPoint(): Point2D = Point2D.Double(x.toDouble(), y.toDouble())
     infix fun isNotBlocking(line: Line) = !line.isOnSegment(this)
+    operator fun minus(other: Coordinate): Coordinate = Coordinate(this.x - other.x, this.y - other.y)
+    fun laserRotationTo(asteroid: Coordinate): Double = (asteroid - this)
+        .let { atan2(it.y.toDouble(), it.x.toDouble()) + 0.5 * PI }
+        .let { if (it < 0) it + 2 * PI else it }.degrees
+
+    override fun toString() = "${x * 100 + y}"
 }
 
 data class Line(val start: Coordinate, val end: Coordinate) {
@@ -18,8 +26,13 @@ data class Line(val start: Coordinate, val end: Coordinate) {
     val width get() = end.x.toDouble() - start.x
 }
 
+data class Target(val asteroid: Coordinate, var rotationDelta: Double)
+
 class Scan(private val asteroids: Set<Coordinate>) {
-    fun nrOfDetectableAsteroidsFromBestLocation(): Int = asteroids.map { visibleAsteroids(it) }.max()!!
+    fun nrOfDetectableAsteroidsFromBestLocation(): Int = bestStationLocation().second
+    private fun bestLocation(): Coordinate = bestStationLocation().first
+    private fun bestStationLocation(): Pair<Coordinate, Int> =
+        asteroids.map { Pair(it, visibleAsteroids(it)) }.maxBy { it.second }!!
 
     private fun visibleAsteroids(station: Coordinate): Int {
         val possiblyVisible = asteroids.filterNot { it == station }
@@ -29,7 +42,27 @@ class Scan(private val asteroids: Set<Coordinate>) {
             possiblyBlocking.all { it isNotBlocking lineOfSight }
         }
     }
+
+    // 1. find station
+    // 2. enrich each target with: rotation-delta = angle between line going from station UP and asteroid and distance from station
+    //      bring asteroid coordinate into station-coordinates (station = 0,0), line up = 0,0->0,-10, line to target = 0,0->(asteroid - station)
+    //      angle = atan2( -dy / dx ) angle can be in RAD
+    //      probably write some unit tests to test angle for some obvious coordinates (check if the are ok)
+    // 3. group by angle, for each group, first gets 0, second +360 (2*PI), third +720, etc added to their rotation-delta
+    // 4. sort by rotation-delta
+    // 5. grab [199]
+    fun findVaporizedAsteroid200(): Coordinate {
+        val station = bestLocation()
+        val targets = asteroids.filterNot { it == station }.map { Target(it, station.laserRotationTo(it)) }
+        targets.groupBy { it.rotationDelta }.filter { it.value.size > 1 }.forEach { (_, targets) ->
+            targets.withIndex().forEach { it.value.rotationDelta += it.index * 360 }
+        }
+        val sortedTargets = targets.sortedBy { it.rotationDelta }
+        return sortedTargets[199].asteroid
+    }
 }
+
+val Double.degrees: Double get() = this * 180 / PI
 
 fun String.toScan(): Scan = Scan(this.trimIndent()
     .lines().mapIndexed { y, line ->
--- test/adventofcode/day10/AsteroidsTest.kt
+++ test/adventofcode/day10/AsteroidsTest.kt
@@ -1,6 +1,7 @@
 package adventofcode.day10
 
 import org.junit.jupiter.api.Test
+import kotlin.math.PI
 import kotlin.math.atan
 import kotlin.test.assertEquals
 
@@ -29,7 +30,54 @@ internal class AsteroidsTest {
 
     @Test
     fun `part 1`() {
-        val scan = """
+        val bestLocation = scan.nrOfDetectableAsteroidsFromBestLocation()
+        assertEquals(309, bestLocation)
+    }
+
+    @Test
+    fun deg() {
+        assertEquals(0.0, 0.0.degrees)
+        assertEquals(90.0, (0.5 * PI).degrees)
+        assertEquals(180.0, (PI).degrees)
+        assertEquals(270.0, (1.5 * PI).degrees)
+        assertEquals(360.0, (2 * PI).degrees)
+        assertEquals(720.0, (4 * PI).degrees)
+    }
+
+    @Test
+    fun coordinateMinus() {
+        val station = Coordinate(2, 2)
+        assertEquals(Coordinate(0, 0), Coordinate(2, 2) - station)
+        assertEquals(Coordinate(0, 2), Coordinate(2, 4) - station)
+        assertEquals(Coordinate(0, -1), Coordinate(2, 1) - station)
+        assertEquals(Coordinate(-2, -1), Coordinate(0, 1) - station)
+    }
+
+    @Test
+    fun laserRotation() {
+        val station = Coordinate(2, 2)
+        assertEquals(0.0, station.laserRotationTo(Coordinate(2, 0)))
+        assertEquals(90.0, station.laserRotationTo(Coordinate(4, 2)))
+        assertEquals(180.0, station.laserRotationTo(Coordinate(2, 4)))
+        assertEquals(270.0, station.laserRotationTo(Coordinate(0, 2)))
+    }
+
+    @Test
+    fun laserRotation2() {
+        val station = Coordinate(37, 25)
+        assertEquals(304.04593735660166, station.laserRotationTo(Coordinate(0, 0)))
+        assertEquals(305.5376777919744, station.laserRotationTo(Coordinate(2, 0)))
+        assertEquals(308.6598082540901, station.laserRotationTo(Coordinate(7, 1)))
+    }
+
+    @Test
+    fun `part 2`() {
+        val vaporizedAsteroid200 = scan.findVaporizedAsteroid200()
+        assertEquals("416", vaporizedAsteroid200.toString())
+    }
+}
+
+val scan = """
             #.#................#..............#......#......
             .......##..#..#....#.#.....##...#.........#.#...
             .#...............#....#.##......................
@@ -79,8 +127,3 @@ internal class AsteroidsTest {
             .#.........#.#.#...#...#....#........#..#.......
             ...#..#.#......................#...............#
         """.toScan()
-
-        val bestLocation = scan.nrOfDetectableAsteroidsFromBestLocation()
-        assertEquals(309, bestLocation)
-    }
-}
